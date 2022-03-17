7d61bb72a53b5af64da6f23e1ca38f47b72ea5b6
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2022-03-17 09:44:22 +0100

    Header | Jumbotron

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+    <header>
+        <div class="container-md">
+            <div class="d-flex justify-content-between align-items-center">
+                <div class=" ">
+                    <h1 class="logo">DELIVEBOO</h1>
+                </div>
+
+                <div class="">
+                    <span class="title main-color">
+                        Accedi
+                    </span>
+                    <span class="title main-color">
+                        Registrati
+                    </span>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../sass/app';
+
+header {
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left:0;
+
+    padding: 15px;
+
+    .title {
+        font-size: 24px;
+        margin: 0 10px;
+    }
+
+    .logo {
+        font-size: 50px;
+    }
+
+
+    }
+
+
+
+</style>
---
+++ resources/js/components/Jumbotron.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="bg-jumbo">
+        <div class="overlay"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Jumbotron'
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../../sass/app';
+  
+    .bg-jumbo {
+        height: 600px;
+        background-image: url('~/images/meat-wallpaper.jpg');
+        background-position-y: bottom;
+        background-size: cover;
+        
+    }
+    .overlay {
+        width: 100%;
+        height: 100%;
+        background: rgba(0,0,0,0);
+        background: linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 65%);
+
+    }
+
+
+</style>
