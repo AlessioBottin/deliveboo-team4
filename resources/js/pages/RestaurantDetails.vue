fc80672628845d1ff93f1f2d6ab056ce9235dc11
salzanopm <salzanopm@gmail.com>
2022-03-21 17:56:16 +0100

    adjusting RestaurantDetails

--- resources/js/pages/RestaurantDetails.vue
+++ resources/js/pages/RestaurantDetails.vue
@@ -1,7 +1,36 @@
 <template>
     <section>
+        <div>
+            <Jumbotron />
+        </div>
         <div class="container">
-            <h1>Restaurant details</h1>
+            <div class="card">
+                <div class="img_container">
+                    <div class="img_box">  
+                        <img src="../../../img-test/download.png" class="card-img-top " alt="...">
+                    </div>
+                </div>
+                
+                <div class="card-body text-center">
+                    <h2 class="card-title">Card title</h2>
+                    <p class="card-text justify-content-center ">Categoria 1 - Categoria 2</p>
+                    <p class="card-text justify-content-center ">Rating stars</p>
+                    <p class="card-text justify-content-center ">Via fasulla 123</p>
+                    
+                    <div class="card-body text-center grey_card">
+                        <div class="d-flex justify-content-between">
+                            <p class="card-text justify-content-center ">Consegna</p>
+                            <p class="card-text justify-content-center ">Voglio ritirare</p>
+                        </div>
+                        <div class="d-flex justify-content-between mt-3">
+                            <p class="card-text justify-content-center ">Consegna gratuita</p>
+                            <p class="card-text justify-content-center "> <strong>20€</strong> ordine minimo</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            
             <!-- <h1>{{ post.title }}</h1>
             <div v-if="post.category">Category: {{ post.category.name }}</div>
             <div v-if="post.tags && post.tags.length > 0">
@@ -21,6 +50,13 @@
 </template>
 
 <script>
+import Jumbotron from '../components/Jumbotron.vue'
+export default {
+    name: 'RestaurantDetails',
+    components: {
+        Jumbotron
+    }
+}
 // export default {
 //     name: 'PostDetails',
 //     data: function() {
@@ -46,3 +82,44 @@
 //     }
 // }
 </script>
+
+<style scoped>
+    li {
+        display: flex;
+        justify-content: center;
+    }
+
+    .img_container {
+        display: flex;
+        justify-content: center;
+    }  
+
+    .container {
+        margin: -150px auto 40px auto;
+        width: 700px;
+       
+    }
+    
+    .img_box {
+        margin: -50px 0 0 0;
+        width: 100px;
+        border: solid white 4px;
+        border-radius: 20px;
+        overflow: hidden;
+    }
+
+    .card {
+        border-radius: 15px;
+    }
+   
+    .grey_card {
+        width: 65%;
+        margin: auto;
+        background-color: #f5f3f1;
+        border-radius: 10px;
+    }
+
+    
+
+   
+</style>
--- resources/js/router.js
+++ resources/js/router.js
@@ -3,35 +3,35 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-import Home from './pages/Home.vue';
-import RestaurantList from './pages/RestaurantList.vue';
+// import Home from './pages/Home.vue';
+// import RestaurantList from './pages/RestaurantList.vue';
 import RestaurantDetails from './pages/RestaurantDetails.vue';
-import Payment from './pages/Payment.vue';
+// import Payment from './pages/Payment.vue';
 import NotFound from './pages/NotFound.vue';
 
 const router = new VueRouter({
     mode: "history",
     routes: [
-        {
-            path: "/",
-            name: "home",
-            component: Home
-        },
-        {
-            path: "/restaurant-list",
-            name: "restaurant-list",
-            component: AdvanceResearch
-        },
+        // {
+        //     path: "/",
+        //     name: "home",
+        //     component: Home
+        // },
+        // {
+        //     path: "/restaurant-list",
+        //     name: "restaurant-list",
+        //     component: AdvanceResearch
+        // },
         {
             path: "/restaurant-details",
             name: "restaurant-details",
             component: RestaurantDetails
         },
-        {
-            path: "/payment",
-            name: "payment",
-            component: Payment
-        },
+        // {
+        //     path: "/payment",
+        //     name: "payment",
+        //     component: Payment
+        // },
         {
             path: "/*",
             name: "not-found",
