ca1b0550618670673837dc5e053132a568f03655
salzanopm <salzanopm@gmail.com>
2022-03-21 18:00:39 +0100

    error router

--- resources/js/router.js
+++ resources/js/router.js
@@ -3,26 +3,15 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-<<<<<<< HEAD
 // import Home from './pages/Home.vue';
 // import RestaurantList from './pages/RestaurantList.vue';
 import RestaurantDetails from './pages/RestaurantDetails.vue';
 // import Payment from './pages/Payment.vue';
-=======
-import Home from './pages/Home.vue';
-import RestaurantList from './pages/RestaurantList.vue';
-<<<<<<< HEAD
-=======
-import RestaurantDetails from './pages/RestaurantDetails.vue';
->>>>>>> a4c66ea4040c4a4c899f4d923ef37e75917d6734
-import Payment from './pages/Payment.vue';
->>>>>>> 9244e61d3b30667740d5b38e5e2c6e684879b533
 import NotFound from './pages/NotFound.vue';
 
 const router = new VueRouter({
     mode: "history",
     routes: [
-<<<<<<< HEAD
         // {
         //     path: "/",
         //     name: "home",
@@ -33,18 +22,6 @@ const router = new VueRouter({
         //     name: "restaurant-list",
         //     component: AdvanceResearch
         // },
-=======
-        {
-            path: "/",
-            name: "home",
-            component: Home
-        },
-        {
-            path: "/restaurant-list",
-            name: "restaurant-list",
-            component: RestaurantList
-        },
->>>>>>> 9244e61d3b30667740d5b38e5e2c6e684879b533
         {
             path: "/restaurant-details",
             name: "restaurant-details",
