e6570ad41bc7dd6729bdbfc2f39d04e15ce60806
turchimatteo <turchi12@gmail.com>
2022-03-21 16:45:34 +0100

    plates seeder

---
+++ config/plates.php
@@ -0,0 +1,69 @@
+<?php
+
+return
+$plates_array = [
+    [
+        'name' => 'Wopper',
+        'ingredients' => 'Pane, Carne, Lattuga, Cipolla, Ketchup, Cetrioli, Pommodoro',
+        'description' => 'La ricetta del leggendario WHOPPER non è cambiata. La succosa carne di manzo alla griglia e gli ingredienti freschi ti garantiscono un sapore ineguagliabile. Naturalmente viene sempre preparato al momento per te, proprio come ti piace di più. È bello che certe cose non cambino mai.',
+        'price' => 4.90,
+        'visibility' => 1,
+        'image' => 'https://www.burgerking.it/site/assets/files/1046/whopper.png',
+        'user_id' => 1,
+    ],
+    [
+        'name' => 'Bacon King',
+        'ingredients' => 'Pane, Carne, Bacon, Maionese, Ketchup, Formaggio',
+        'description' => 'Il panino definitivo. La combinazione perfetta di ingredienti. Il gusto sublime del bacon e quello della carne alla griglia, accompagnati da ketchup, maionese e saporito pane al mais: il Bacon King è un panino che non si fa dire di no. Provalo in tutti i nostri ristoranti: ti innamorerai!',
+        'price' => 5.90,
+        'visibility' => 1,
+        'image' => 'https://www.burgerking.it/site/assets/files/8989841/singolo_prodotto_1350x1515.png',
+        'user_id' => 1,
+    ],
+    [
+        'name' => 'Patatine',
+        'ingredients' => 'Patata, Sale',
+        'description' => 'Le nostre Patatine KING sono degne di un re: deliziose patate, colore dorato e croccantezza sono le caratteristiche che le rendono uniche.',
+        'price' => 3.00,
+        'visibility' => 1,
+        'image' => 'https://www.burgerking.it/site/assets/files/2039/patatine_1350x1515_m.png',
+        'user_id' => 1,
+    ],
+    [
+        'name' => 'Margherita',
+        'ingredients' => 'Impasto pizza, Pommodoro, Mozzarella, Basilico',
+        'description' => 'Il classico per eccellenza, la nostra pizza più amata. Pomodoro, mozzarella filante, sale e olio quanto basta, da mangiare fino all\'ultimo boccone.',
+        'price' => 8.50,
+        'visibility' => 1,
+        'image' => 'https://d1e3z2jco40k3v.cloudfront.net/-/media/drog19/recipe-images/pizza-margherita-con-basilico_2000.jpg?rev=c0ef67e2f4684f9dbbcf1a54188cc5b0&vd=20200707T052020Z&hash=097A78228B5887F0247E9EFAB27CE958',
+        'user_id' => 2,
+    ],
+    [
+        'name' => 'Patate e Salsiccia',
+        'ingredients' => 'Impasto pizza, Mozzarella, Patate, Salsiccia, Rosmarino',
+        'description' => 'Un\'esplosione di sapori per una ricetta ricca e gustosa. che conquista chi ama i sapori rustici ed intensi.',
+        'price' => 8.90,
+        'visibility' => 1,
+        'image' => 'https://www.dueamicheincucina.it/wp-content/uploads/2019/09/Pizza-patate-e-salsiccia-1-1024x682.jpg',
+        'user_id' => 2,
+    ],
+    [
+        'name' => 'Classic Fillet',
+        'ingredients' => 'Filetto di Manzo, Patate, Salsa OWW',
+        'description' => 'Filetto di Manzo alla griglia servito con patate French Fries e salsa OWW',
+        'price' => 18.50,
+        'visibility' => 1,
+        'image' => 'https://www.oldwildwest.it/proxyvfs.axd/img_main/r21121/squak-steak-jpg?v=16559&ext=.jpg',
+        'user_id' => 4,
+    ],
+    [
+        'name' => 'Bucket Tender Crispy',
+        'ingredients' => 'Pollo fritto',
+        'description' => 'Un Bucket con 12 deliziosi Tender Crispy per 2 persone e con ben 3 salse a scelta. Condividerlo non sarà facile ma è per 2 persone! I Tender Crispy sono infarinati due volte, rigorosamente a mano, e fritti al momento ogni giorno nei nostri ristoranti. È questo che rende li rende croccantissimi fuori e tenerissimi dentro. Non vi resta che scegliere le vostre salse preferite e immergervi nel gusto! Provate il Bucket Tender Crispy anche nella versione Menu medio o grande con bibite e contorni medi o grandi a scelta.',
+        'price' => 7.50,
+        'visibility' => 1,
+        'image' => 'https://media.kfc.it/app/uploads/public/5ec/f7e/777/5ecf7e7776a01316803887.jpg',
+        'user_id' => 3,
+    ],
+]
+?>
