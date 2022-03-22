fad03aaf578f2c45587a4730a16b41570c75a117
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2022-03-22 10:43:02 +0100

    Category Banner

---
+++ resources/js/components/Category.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="d-flex justify-content-lg-center mt-5">
+        <div class="row row-xl-cols-6 row-md-cols-3 row-sm-cols-2 justify-content-start">
+            <div class="col test">
+                <div class="card" style="width: 9rem;">
+                    <img src="https://banner2.cleanpng.com/20180424/cgw/kisspng-mcdonald-s-big-mac-hamburger-big-n-tasty-cheesebu-mcdonalds-5adef6fb6fd888.4092768115245616594581.jpg" class="card-img-top" alt="">
+                    <div class="card-body p-0" style="height: 2rem;">
+                        <p class="card-title text-center">Categoria</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col test">
+                <div class="card" style="width: 9rem;">
+                    <img src="https://banner2.cleanpng.com/20180424/cgw/kisspng-mcdonald-s-big-mac-hamburger-big-n-tasty-cheesebu-mcdonalds-5adef6fb6fd888.4092768115245616594581.jpg" class="card-img-top" alt="">
+                    <div class="card-body p-0" style="height: 2rem;">
+                        <p class="card-title text-center">Categoria</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col test">
+                <div class="card" style="width: 9rem;">
+                    <img src="https://banner2.cleanpng.com/20180424/cgw/kisspng-mcdonald-s-big-mac-hamburger-big-n-tasty-cheesebu-mcdonalds-5adef6fb6fd888.4092768115245616594581.jpg" class="card-img-top" alt="">
+                    <div class="card-body p-0" style="height: 2rem;">
+                        <p class="card-title text-center">Categoria</p>
+                    </div>
+                </div>
+            </div>    
+
+            <div class="col test">
+                <div class="card" style="width: 9rem;">
+                    <img src="https://banner2.cleanpng.com/20180424/cgw/kisspng-mcdonald-s-big-mac-hamburger-big-n-tasty-cheesebu-mcdonalds-5adef6fb6fd888.4092768115245616594581.jpg" class="card-img-top" alt="">
+                    <div class="card-body p-0" style="height: 2rem;">
+                        <p class="card-title text-center">Categoria</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col test">
+                <div class="card" style="width: 9rem;">
+                    <img src="https://banner2.cleanpng.com/20180424/cgw/kisspng-mcdonald-s-big-mac-hamburger-big-n-tasty-cheesebu-mcdonalds-5adef6fb6fd888.4092768115245616594581.jpg" class="card-img-top" alt="">
+                    <div class="card-body p-0" style="height: 2rem;">
+                        <p class="card-title text-center">Categoria</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col test">
+                <div class="card" style="width: 9rem;">
+                    <img src="https://banner2.cleanpng.com/20180424/cgw/kisspng-mcdonald-s-big-mac-hamburger-big-n-tasty-cheesebu-mcdonalds-5adef6fb6fd888.4092768115245616594581.jpg" class="card-img-top" alt="">
+                    <div class="card-body p-0" style="height: 2rem;">
+                        <p class="card-title text-center">Categoria</p>
+                    </div>
+                </div>
+            </div>    
+        </div>
+    </div>
+    
+</template>
+
+<script>
+
+export default {
+    name: 'Category',
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../sass/app';
+
+
+.test {
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+}
+
+
+</style>
---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Header />
+        <Jumbotron />
+        <Category />
+    </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue';
+import Jumbotron from '../components/Jumbotron.vue';
+import Category from '../components/Category.vue';
+
+export default {
+    name: 'Home',
+    components: {
+        Header,
+        Jumbotron,
+        Category,
+    }
+}
+</script>
