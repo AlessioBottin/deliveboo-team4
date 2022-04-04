648a15c669045eba5512fe0cd80f139e4ac07adf
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2022-04-04 17:46:30 +0200

    mail customer e restaurant

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+// use Illuminate\Support\Facades\Mail;
+// use App\Mail\NewOrderRestaurantNotification;
 use App\Order;
 use App\Plate;
 
@@ -52,6 +54,11 @@ class OrderController extends Controller
 
         return view('admin.orders.show',$data);
     }
+
+
+    // public function store(Users $users) {
+    //     Mail::to('$users->email')->send(new NewOrderRestaurantNotification($new_order));
+    // }
 
     public function statistics() {
         $php_orders = Order::getAllMyOrders();
